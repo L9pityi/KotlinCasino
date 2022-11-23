70ccff04e42c2518010ff7432b11ee621cc816fa
martonistvan0330 <mistvan0330@gmail.com>
2022-11-23 02:45:38 +0100

    Create Rank.kt

---
+++ Casino/src/main/kotlin/casino/blackjack/Rank.kt
@@ -0,0 +1,17 @@
+package casino.blackjack
+
+enum class Rank {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
